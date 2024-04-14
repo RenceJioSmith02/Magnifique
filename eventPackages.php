93e53728e759aab0a0d807044de6a2c45a09c9b6
dannnverrrr <166786008+dannnverrrr@users.noreply.github.com>
2024-04-14 22:24:41 +0800

    title at fav

--- eventPackages.php
+++ eventPackages.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Parallax Effect | Codehal</title>
+    <link rel="icon" type="image/x-icon" href="img/bg&icons/logo.png">
+    <title>MAGNIFIQUE Events & Co.</title>
 
     <!-- css -->
     <link rel="stylesheet" href="css/eventPackages.css">
--- index.php
+++ index.php
@@ -3,7 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="icon" type="image/x-icon" href="img/bg&icons/logo.png">
+    <title>MAGNIFIQUE Events & Co.</title>
+    
 
     <!-- css -->
     <link rel="stylesheet" href="css/homepage.css">
